66d93181fb2f9bca80ec95b55c864b56b0aae393
Ritch Melton <Ritch Melton@localhost>
2013-03-25 18:40:24 -0400

    Added fourier transform.

---
+++ BeatDetector/ComplexNumber.cs
@@ -0,0 +1,46 @@
+﻿namespace BeatDetector
+{
+    public class ComplexNumber
+    {
+        public double Real { get; private set; }
+        public double Imaginary { get; private set; }
+
+        public static readonly ComplexNumber Zero = new ComplexNumber();
+
+        public ComplexNumber()
+        {
+            Real = new double();
+            Imaginary = new double();
+        }
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public ComplexNumber Add(ComplexNumber right)
+        {
+            Real += right.Real;
+            Imaginary += right.Imaginary;
+
+            return this;
+        }
+
+        public ComplexNumber Subtract(ComplexNumber right)
+        {
+            Real -= right.Real;
+            Imaginary -= right.Imaginary;
+
+            return this;
+        }
+
+        public ComplexNumber Multiply(ComplexNumber right)
+        {
+            Real = Real * right.Real - Imaginary * right.Imaginary;
+            Imaginary = Real * right.Imaginary + Imaginary * right.Real;
+
+            return this;
+        }
+    }
+}
